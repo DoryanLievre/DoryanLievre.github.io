adb4f4d759b56636837eef9941abf9c7407cd081
DoryanLievre <dlievre.dev@gmail.com>
2021-05-02 18:57:35 +0200

    typed_text effects

---
+++ js/typed_text.js
@@ -0,0 +1,19 @@
+function randomiseDelay(min,max)
+{
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+function typeWrite(el){
+  el.addClass('cursor')
+  var text = el.data('string');
+  var delay = parseInt(el.data('delay'));
+  var randInt = 0;
+  for (var i = 0; i < text.length; i++) {
+    randInt += parseInt(randomiseDelay(50,150));
+    var typing = setTimeout(function(y){
+      el.append(text.charAt(y));
+    },randInt, i);
+  };
+}
+
+typeWrite($(".typeme"));
